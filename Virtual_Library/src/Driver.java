1a90ef7ee9937fed718093ff9926e64a9edbb908
matt-oak <matt.oak@live.com>
2017-04-23 14:23:53 -0600

    PostgreSQL query fix

--- Virtual_Library/src/Driver.java
+++ Virtual_Library/src/Driver.java
@@ -1,4 +1,8 @@
 import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 
 public class Driver 
@@ -17,10 +21,10 @@ public class Driver
 		}
 		else
 		{
-			createAccount();
+			User user = createAccount();
 		}
 	}
-	public static void createAccount()
+	public static User createAccount()
 	{
 		//Get user-input
 		Scanner input = new Scanner(System.in);
@@ -34,14 +38,36 @@ public class Driver
 		
 		//Create User object
 		User user = new User(email, username, pass);
+		return user;
 	}
-	
+	public static void searchBook(){
+		Connection c = null;
+		Statement stmt = null;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Book Name:");
+		String book = input.nextLine();
+		try{
+			Class.forName("org.postgresql.Driver");
+			c = DriverManager.
+					getConnection("jdbc:postgresql://localhost:5432/library", "test", "pass");
+			c.setAutoCommit(false);
+			stmt = c.createStatement();
+	        ResultSet rs = stmt.executeQuery( "SELECT * FROM library WHERE \"Title\" = \'" + book + "\';");
+	        if (rs.next()){
+	        	String author = rs.getString("Author");
+		        System.out.println("Author: " + author);
+	        }
+		}
+		catch (Exception e){
+			System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+		}
+	}
 	
 
 	public static void main(String[] args) 
 	{
 		initialize();
-		
+		searchBook();
 	}
 
 }
