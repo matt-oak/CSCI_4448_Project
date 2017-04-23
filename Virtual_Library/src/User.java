907edd1f94a407a93e4834cef39f20a7fec1a347
cocr1742 <cory.cranford@colorado.edu>
2017-04-23 16:24:06 -0600

    Added isAdmin boolean

--- Virtual_Library/src/User.java
+++ Virtual_Library/src/User.java
@@ -3,12 +3,14 @@ public class User
 	private String email;
 	private String username;
 	private String password;
+	private Boolean isAdmin;
 	
-	public User(String email, String username, String password)
+	public User(String email, String username, String password, Boolean isAdmin)
 	{
 		this.email = email;
 		this.username = username;
 		this.password = password;
+		this.isAdmin = isAdmin;
 	}
 	
 	public void setEmail(String email)
@@ -26,6 +28,11 @@ public class User
 		this.password = password;
 	}
 	
+	public void setIsAdmin(Boolean isAdmin)
+	{
+		this.isAdmin = isAdmin;
+	}
+	
 	public String getEmail()
 	{
 		return email;
@@ -39,5 +46,10 @@ public class User
 	public String getPassword()
 	{
 		return password;
+	}
+	
+	public Boolean getIsAdmin()
+	{
+		return isAdmin;
 	}
 }
