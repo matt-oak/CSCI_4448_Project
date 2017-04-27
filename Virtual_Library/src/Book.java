45cd63dbce39017cf003dad9317e6008f11beab8
Danny <dath7892@colorado.edu>
2017-04-27 17:26:08 -0600

    Added some ratings functionality, but is untested with the DB

--- Virtual_Library/src/Book.java
+++ Virtual_Library/src/Book.java
@@ -8,14 +8,16 @@ public class Book {
 	private String genre; 
 	private Integer pageCount;
 	private List<String> comments;
+	private List<String> ratings;
 	
-	public Book(String title, String author, String genre, Integer pageCount, List<String> comments)
+	public Book(String title, String author, String genre, Integer pageCount, List<String> comments, List<String> ratings)
 	{
 		this.title = title;
 		this.author = author;
 		this.genre = genre;
 		this.pageCount = pageCount;
 		this.comments = new ArrayList<String>();
+		this.ratings = new ArrayList<String>();
 	}
 
 	public void setTitle(String title)
@@ -37,6 +39,11 @@ public class Book {
 	public void setGenre(String genre)
 	{
 		this.genre = genre;
+	}
+	
+	public void setRatings(List<String> ratings)
+	{
+		this.ratings = ratings;
 	}
 
 	public void setPageCount(Integer pageCount)
@@ -68,20 +75,30 @@ public class Book {
 	{
 		return comments.get(0);
 	}
+	
+	public String getRatings()
+	{
+		return ratings.get(0);
+	}
 
 	public void addComment(String comment)
 	{
 		List<String> comments = this.comments;
 		comments.add(comment);
+		/// This should be a list of Comment objects eventually
+		
 	}
-	public void addRating()
+	public void addRating(String rating)
 	{
-
+		List<String> ratings = this.ratings;
+		ratings.add(rating);
+		/// This should be a list of Rating objects eventually
+		
 	}
 
 	public void addFavorite()
 	{
-
+		
 	}
 
 	public void back()
--- Virtual_Library/src/User.java
+++ Virtual_Library/src/User.java
@@ -1,5 +1,6 @@
 public class User 
 {
+	private int userID;
 	private String email;
 	private String username;
 	private String password;
@@ -7,10 +8,16 @@ public class User
 	
 	public User(String email, String username, String password, Boolean isAdmin)
 	{
+		//this.userID = userID;
 		this.email = email;
 		this.username = username;
 		this.password = password;
 		this.isAdmin = isAdmin;
+	}
+	
+	public void setUserID(int id)
+	{
+		this.userID = id;
 	}
 	
 	public void setEmail(String email)
@@ -31,6 +38,11 @@ public class User
 	public void setIsAdmin(Boolean isAdmin)
 	{
 		this.isAdmin = isAdmin;
+	}
+	
+	public int getUserID()
+	{
+		return userID;
 	}
 	
 	public String getEmail()
